7e503f8c0868c84beb7745549c7adc4c7ab8d68c
Eshan-mishra <eshanmishra1@outlook.com>
2025-04-18 14:18:30 +0530

    Refactor app.js for improved readability and add new dependencies

---
+++ models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String, //clodinary
+      required: true,
+    },
+    thumbnail: {
+      type: String, //clodinary
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    discription: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    }
+  },
+  { timestapms: true }
+);
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema);
